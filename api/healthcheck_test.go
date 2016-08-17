e2a3b4c6d30c6e06729f6a4ab797d00fc26ab374
Mauricio Banduk <mauricio.banduk@topfreegames.com>
2016-08-17 14:38:21 -0300

    Add healthcheck tests

--- api/healthcheck_test.go
+++ api/healthcheck_test.go
@@ -1,33 +1,39 @@
 package api_test
 
-// import (
-// 	"net/http"
-// 	"testing"
-//
-// 	. "github.com/franela/goblin"
-// 	. "github.com/onsi/gomega"
-// )
-//
-// func TestHealthcheckHandler(t *testing.T) {
-// 	g := Goblin(t)
-//
-// 	// special hook for gomega
-// 	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
-//
-// 	g.Describe("Healthcheck Handler", func() {
-// 		g.It("Should respond with default WORKING string", func() {
-// 			a := GetDefaultTestApplication()
-// 			res := Get(a, "/healthcheck", t)
-//
-// 			res.Status(http.StatusOK).Body().Equal("WORKING")
-// 		})
-//
-// 		g.It("Should respond with customized WORKING string", func() {
-// 			a := GetDefaultTestApplication()
-// 			a.Config.SetDefault("healthcheck.workingText", "OTHERWORKING")
-// 			res := Get(a, "/healthcheck", t)
-//
-// 			res.Status(http.StatusOK).Body().Equal("OTHERWORKING")
-// 		})
-// 	})
-// }
+import (
+	"net/http"
+
+	"git.topfreegames.com/topfreegames/marathon/models"
+	mt "git.topfreegames.com/topfreegames/marathon/testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/uber-go/zap"
+)
+
+var _ = Describe("Marathon API Handler", func() {
+	var (
+		l zap.Logger
+	)
+	BeforeEach(func() {
+		l = mt.NewMockLogger()
+		_db, err := models.GetTestDB(l)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(_db).NotTo(BeNil())
+	})
+
+	Describe("Create Notification Handler", func() {
+		It("Should respond with default WORKING string", func() {
+			a := GetDefaultTestApp()
+			res := Get(a, "/healthcheck")
+
+			res.Status(http.StatusOK).Body().Equal("working")
+		})
+		It("Should respond with customized WORKING string", func() {
+			a := GetDefaultTestApp()
+			a.Config.Set("healthcheck.workingText", "OTHERWORKING")
+			res := Get(a, "/healthcheck")
+
+			res.Status(http.StatusOK).Body().Equal("OTHERWORKING")
+		})
+	})
+})
