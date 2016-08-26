b7b3709bc03a5927ed80d8cc09a88a33f0056a38
cscatolini <camila.scatolini@gmail.com>
2016-08-26 18:31:57 -0300

    Stop with the fucking %2f -> MISSING errors!!!

--- api/upload.go
+++ api/upload.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"net/url"
 	"time"
 
 	"github.com/kataras/iris"
@@ -24,7 +25,7 @@ func UploadHandler(application *Application) func(c *iris.Context) {
 		s3SecretAccessKey := application.Config.GetString("s3.secretAccessKey")
 		enableSSL := true
 
-		s3Key := fmt.Sprintf("%s/upload-%v.csv", s3Folder, start.Unix())
+		s3Key := fmt.Sprintf("%s/upload%v.csv", s3Folder, start.Unix())
 		s3Client, err := minio.New("s3.amazonaws.com", s3AccessKeyID, s3SecretAccessKey, enableSSL)
 		if err != nil {
 			l.Error(
@@ -36,7 +37,7 @@ func UploadHandler(application *Application) func(c *iris.Context) {
 			return
 		}
 
-		url, err := s3Client.PresignedPutObject(s3Bucket, s3Key, s3DaysExpiry)
+		u, err := s3Client.PresignedPutObject(s3Bucket, s3Key, s3DaysExpiry)
 		if err != nil {
 			l.Error(
 				"Failed to create presigned PUT policy.",
@@ -46,18 +47,23 @@ func UploadHandler(application *Application) func(c *iris.Context) {
 			FailWith(400, err.Error(), c)
 			return
 		}
-		params := make(map[string]string)
-		params["url"] = url.String()
-
 		l.Info(
 			"Upload handler retrieved url and params successfully.",
 			zap.Duration("duration", time.Now().Sub(start)),
 		)
 		m := make(map[string]interface{})
-		for k, v := range params {
-			m[k] = v
+		// For some reason we need to unescape it
+		uURL, err := url.QueryUnescape(u.String())
+		if err != nil {
+			l.Error(
+				"Failed to unescape presigned URL.",
+				zap.Error(err),
+				zap.Duration("duration", time.Now().Sub(start)),
+			)
+			FailWith(500, err.Error(), c)
+			return
 		}
-
+		m["url"] = uURL
 		SucceedWith(m, c)
 	}
 }
