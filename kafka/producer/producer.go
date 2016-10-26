0f7e2e2cf857be6680a58e4f3346289d56d6f987
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2016-10-26 15:32:30 -0200

    getting brokers from zookeeper

--- kafka/producer/producer.go
+++ kafka/producer/producer.go
@@ -2,10 +2,12 @@ package producer
 
 import (
 	"strings"
+	"time"
 
 	"git.topfreegames.com/topfreegames/marathon/log"
 	"git.topfreegames.com/topfreegames/marathon/messages"
 	"github.com/Shopify/sarama"
+	"github.com/samuel/go-zookeeper/zk"
 	"github.com/spf13/viper"
 	"github.com/uber-go/zap"
 )
@@ -14,9 +16,27 @@ import (
 func Producer(l zap.Logger, config *viper.Viper, inChan <-chan *messages.KafkaMessage, doneChan <-chan struct{}) {
 	log.I(l, "Starting producer")
 	saramaConfig := sarama.NewConfig()
+	//TODO: replace
 	brokersStr := config.GetString("workers.producer.brokers")
 	brokers := strings.Split(brokersStr, ",")
 	producer, err := sarama.NewSyncProducer(brokers, saramaConfig)
+	zookeeperHosts := config.GetStringSlice("workers.zookeeperHosts")
+	c, _, err := zk.Connect(zookeeperHosts, time.Second*10)
+	if err != nil {
+		log.P(l, "error connecting to zookeeper", func(cm log.CM) {
+			cm.Write(
+				zap.Object("zookeeperHosts", zookeeperHosts),
+			)
+		})
+	}
+	children, _, err := c.Children("/brokers/ids")
+	if err != nil {
+		panic(err)
+	}
+	log.D(l, "got brokers from zookeeper", func(cm log.CM) {
+		cm.Write(zap.Object("brokers", children))
+	})
+	//
 	if err != nil {
 		log.E(l, "Failed to start kafka producer", func(cm log.CM) {
 			cm.Write(zap.Error(err))
