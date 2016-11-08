aab3d81fbe9a9246acf3c0bbd612a9ec187f2ca8
cscatolini <camila.scatolini@gmail.com>
2016-11-08 16:02:18 -0200

    Rearrange app tests

--- test/unit/api/handlers/appTest.js
+++ test/unit/api/handlers/appTest.js
@@ -101,7 +101,7 @@ describe('Handlers', () => {
           expect(body.data[0]['user-email']).to.contain('empty')
         })
 
-        const tests = [
+        let tests = [
           { args: 'key' },
           { args: 'bundleId' },
         ]
@@ -121,32 +121,9 @@ describe('Handlers', () => {
             expect(body.data[0][test.args]).to.contain('empty')
           })
         })
-      })
 
-      it('invalid user-email header', async function () {
-        const res = await this.request.post('/apps').send(app).set('user-email', 'not an email')
-        expect(res.status).to.equal(422)
-
-        const body = res.body
-        expect(body).to.be.an('object')
-
-        expect(body.data).to.exist()
-        expect(body.data).to.have.length(1)
-        expect(body.data[0]).to.have.property('user-email')
-        expect(body.data[0]['user-email']).to.contain('email format')
-      })
-
-      const tests = [
-        { args: 'key', invalidParam: '', reason: 'empty' },
-        { args: 'key', invalidParam: 'a'.repeat(256), reason: 'length must equal or less than' },
-        { args: 'bundleId', invalidParam: '', reason: 'empty' },
-        { args: 'bundleId', invalidParam: 'a.s', reason: 'bad format.' },
-      ]
-
-      tests.forEach((test) => {
-        it(`invalid ${test.args}`, async function () {
-          app[test.args] = test.invalidParam
-          const res = await this.request.post('/apps').send(app).set('user-email', userEmail)
+        it('invalid user-email header', async function () {
+          const res = await this.request.post('/apps').send(app).set('user-email', 'not an email')
           expect(res.status).to.equal(422)
 
           const body = res.body
@@ -154,8 +131,31 @@ describe('Handlers', () => {
 
           expect(body.data).to.exist()
           expect(body.data).to.have.length(1)
-          expect(body.data[0]).to.have.property(test.args)
-          expect(body.data[0][test.args]).to.contain(test.reason)
+          expect(body.data[0]).to.have.property('user-email')
+          expect(body.data[0]['user-email']).to.contain('email format')
+        })
+
+        tests = [
+          { args: 'key', invalidParam: '', reason: 'empty' },
+          { args: 'key', invalidParam: 'a'.repeat(256), reason: 'length must equal or less than' },
+          { args: 'bundleId', invalidParam: '', reason: 'empty' },
+          { args: 'bundleId', invalidParam: 'a.s', reason: 'bad format.' },
+        ]
+
+        tests.forEach((test) => {
+          it(`invalid ${test.args}`, async function () {
+            app[test.args] = test.invalidParam
+            const res = await this.request.post('/apps').send(app).set('user-email', userEmail)
+            expect(res.status).to.equal(422)
+
+            const body = res.body
+            expect(body).to.be.an('object')
+
+            expect(body.data).to.exist()
+            expect(body.data).to.have.length(1)
+            expect(body.data[0]).to.have.property(test.args)
+            expect(body.data[0][test.args]).to.contain(test.reason)
+          })
         })
       })
     })
@@ -227,7 +227,7 @@ describe('Handlers', () => {
           expect(res.status).to.equal(404)
         })
 
-        const tests = [
+        let tests = [
           { args: 'key' },
           { args: 'bundleId' },
         ]
@@ -247,28 +247,28 @@ describe('Handlers', () => {
             expect(body.data[0][test.args]).to.contain('empty')
           })
         })
-      })
 
-      const tests = [
-        { args: 'key', invalidParam: '', reason: 'empty' },
-        { args: 'key', invalidParam: 'a'.repeat(256), reason: 'length must equal or less than' },
-        { args: 'bundleId', invalidParam: '', reason: 'empty' },
-        { args: 'bundleId', invalidParam: 'a.s', reason: 'bad format.' },
-      ]
+        tests = [
+          { args: 'key', invalidParam: '', reason: 'empty' },
+          { args: 'key', invalidParam: 'a'.repeat(256), reason: 'length must equal or less than' },
+          { args: 'bundleId', invalidParam: '', reason: 'empty' },
+          { args: 'bundleId', invalidParam: 'a.s', reason: 'bad format.' },
+        ]
 
-      tests.forEach((test) => {
-        it(`invalid ${test.args}`, async function () {
-          app[test.args] = test.invalidParam
-          const res = await this.request.put(`/apps/${existingApp.id}`).send(app)
-          expect(res.status).to.equal(422)
+        tests.forEach((test) => {
+          it(`invalid ${test.args}`, async function () {
+            app[test.args] = test.invalidParam
+            const res = await this.request.put(`/apps/${existingApp.id}`).send(app)
+            expect(res.status).to.equal(422)
 
-          const body = res.body
-          expect(body).to.be.an('object')
+            const body = res.body
+            expect(body).to.be.an('object')
 
-          expect(body.data).to.exist()
-          expect(body.data).to.have.length(1)
-          expect(body.data[0]).to.have.property(test.args)
-          expect(body.data[0][test.args]).to.contain(test.reason)
+            expect(body.data).to.exist()
+            expect(body.data).to.have.length(1)
+            expect(body.data[0]).to.have.property(test.args)
+            expect(body.data[0][test.args]).to.contain(test.reason)
+          })
         })
       })
     })
