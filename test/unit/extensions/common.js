f53b9f42b844ee343c8cd12259dfe57df91e02a7
cscatolini <camila.scatolini@gmail.com>
2016-11-08 16:02:24 -0200

    Add license where missing.

--- test/unit/extensions/common.js
+++ test/unit/extensions/common.js
@@ -1,3 +1,9 @@
+// marathon
+// https://github.com/topfreegames/marathon
+// Licensed under the MIT license:
+// http://www.opensource.org/licenses/mit-license
+// Copyright © 2016 Top Free Games <backend@tfgco.com>
+
 import { expect as exp, chai as chaiMod } from '../../common'
 import config from 'config'  //eslint-disable-line
 
