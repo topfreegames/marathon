5f13f239ff5e85c1ec28bc749519cf0c469c6ba1
cscatolini <camila.scatolini@gmail.com>
2016-12-28 18:14:55 -0200

    Travis should be able to push to docker.

--- api/version.go
+++ api/version.go
@@ -23,4 +23,4 @@
 package api
 
 // VERSION identifies marathon's current version
-var VERSION = "0.1.0"
+var VERSION = "0.5.0"
---
+++ push_to_docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+VERSION=$(cat ./api/version.go | grep "var VERSION" | awk ' { print $4 } ' | sed s/\"//g)
+COMMIT=$(git rev-parse HEAD)
+
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+docker build -t marathon .
+docker tag marathon:latest tfgco/marathon:$TRAVIS_BUILD_NUMBER-v$VERSION-$COMMIT
+docker tag marathon:latest tfgco/marathon:$VERSION
+docker tag marathon:latest tfgco/marathon:latest
+docker push tfgco/marathon:$VERSION.$TRAVIS_BUILD_NUMBER
+docker push tfgco/marathon:$VERSION
+docker push tfgco/marathon:latest
+
+DOCKERHUB_LATEST=$(python get_latest_tag.py)
+
+if [ "$DOCKERHUB_LATEST" != "$VERSION.$TRAVIS_BUILD_NUMBER" ]; then
+    echo "Last version is not in docker hub!"
+    echo "docker hub: $DOCKERHUB_LATEST, expected: $VERSION.$TRAVIS_BUILD_NUMBER"
+    exit 1
+fi
