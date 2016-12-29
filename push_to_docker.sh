f87ab2a8eac971b645504df207b839e9da303eaa
cscatolini <camila.scatolini@gmail.com>
2016-12-29 12:37:17 -0200

    Add v to version tag.

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -7,8 +7,8 @@ docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 docker build -t marathon .
 docker tag marathon:latest tfgco/marathon:$TRAVIS_BUILD_NUMBER-v$VERSION-$COMMIT
-docker tag marathon:latest tfgco/marathon:$VERSION
+docker tag marathon:latest tfgco/marathon:v$VERSION
 docker tag marathon:latest tfgco/marathon:latest
 docker push tfgco/marathon:$TRAVIS_BUILD_NUMBER-v$VERSION-$COMMIT
-docker push tfgco/marathon:$VERSION
+docker push tfgco/marathon:v$VERSION
 docker push tfgco/marathon:latest
