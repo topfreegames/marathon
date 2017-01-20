15da6a195adc2accc8ae9289e27d318d886dff27
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2017-01-20 20:04:25 -0200

    fix push to docker

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$LIBRDKAFKA_VERSION" -ne "master" ]; then
+if [ "$LIBRDKAFKA_VERSION" != "master" ]; then
   VERSION=$(cat ./api/version.go | grep "var VERSION" | awk ' { print $4 } ' | sed s/\"//g)
   COMMIT=$(git rev-parse --short HEAD)
 
