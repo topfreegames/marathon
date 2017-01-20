9ae26ee969e7aca63ab494e1b1d9464c186d03c7
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2017-01-20 15:31:47 -0200

    only push to docker if not testing with master librdkafka

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-VERSION=$(cat ./api/version.go | grep "var VERSION" | awk ' { print $4 } ' | sed s/\"//g)
-COMMIT=$(git rev-parse --short HEAD)
+if [ "$LIBRDKAFKA_VERSION" -ne "master" ]; then
+  VERSION=$(cat ./api/version.go | grep "var VERSION" | awk ' { print $4 } ' | sed s/\"//g)
+  COMMIT=$(git rev-parse --short HEAD)
 
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
-docker build -t marathon .
-docker tag marathon:latest tfgco/marathon:$TRAVIS_BUILD_NUMBER-v$VERSION-$COMMIT
-docker tag marathon:latest tfgco/marathon:v$VERSION
-docker tag marathon:latest tfgco/marathon:latest
-docker push tfgco/marathon:$TRAVIS_BUILD_NUMBER-v$VERSION-$COMMIT
-docker push tfgco/marathon:v$VERSION
-docker push tfgco/marathon:latest
+  docker build -t marathon .
+  docker tag marathon:latest tfgco/marathon:$TRAVIS_BUILD_NUMBER-v$VERSION-$COMMIT
+  docker tag marathon:latest tfgco/marathon:v$VERSION
+  docker tag marathon:latest tfgco/marathon:latest
+  docker push tfgco/marathon:$TRAVIS_BUILD_NUMBER-v$VERSION-$COMMIT
+  docker push tfgco/marathon:v$VERSION
+  docker push tfgco/marathon:latest
+fi
